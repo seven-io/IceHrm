022de48d075e54f01ed60cbda22e3ce542a0ad8c
André Matthies <matthiez@gmail.com>
2025-04-28 10:12:48 +0200

    remove obsolete voice parameter xml

--- sevenvoice/web/index.php
+++ sevenvoice/web/index.php
@@ -14,16 +14,9 @@ include_once APP_BASE_PATH . 'modulejslibs.inc.php';
         <?php Util::renderFilters(); ?>
 
         <div class='row form-group'>
-            <div class='col-md-6 no-padding'>
+            <div class='col-md-12 no-padding'>
                 <label for='seven_from'>From</label>
                 <input class='form-control' id='seven_from' maxlength='16' name='from'/>
-            </div>
-
-            <div class='col-md-6 no-padding'>
-                <label>
-                    XML<br>
-                    <input name='xml' type='checkbox' value='1'/>
-                </label>
             </div>
         </div>
 
